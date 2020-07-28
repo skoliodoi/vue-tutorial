860b6bc889866e9903466e6aacf82d1ed8e621a8
Szymon Zygaluk <szymon.zygaluk@gmail.com>
2020-07-28 19:20:54 +0200

    Intro to components

--- Module 5/app.js
+++ Module 5/app.js
@@ -3,6 +3,10 @@ var data = {
   showParagraph: false
 }
 
+Vue.component('hello', {
+  template: '<h1>Hello!</h1>'
+})
+
 var vm1 = new Vue({
   data: data,
   methods: {
@@ -48,7 +52,9 @@ var vm2 = new Vue({
 });
 
 var vm3 = new Vue({
+  el: 'hello',
   template: '<h1>Hello!</h1>'
 })
 
-vm3.$mount('#app3')
+vm3.$mount();
+document.getElementById('app3').appendChild(vm3.$el);
