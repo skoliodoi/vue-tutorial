2cb3beb6450f18d0c3a7adda00021d4435f30ad7
Szymon Zygaluk <szymon.zygaluk@gmail.com>
2020-07-31 12:37:08 +0200

    First steps in the vue-cli environment

--- Module 7.5/src/App.vue
+++ Module 7.5/src/App.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Components</h1>
-
+                <p>This task is {{ status }}</p>
             </div>
         </div>
     </div>
@@ -11,6 +11,11 @@
 
 <script>
     export default {
+      data: function() {
+        return {
+          status: 'completed!'
+        }
+      }
     }
 </script>
 
