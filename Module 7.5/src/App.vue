1d5da620b06587f57abdfa029af66e9db95401bb
Szymon Zygaluk <szymon.zygaluk@gmail.com>
2020-07-31 12:47:48 +0200

    Creating a Vue component

--- Module 7.5/src/App.vue
+++ Module 7.5/src/App.vue
@@ -1,12 +1,5 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <h1>Components</h1>
-                <p>This task is {{ status }}</p>
-            </div>
-        </div>
-    </div>
+    <app-server-status></app-server-status>
 </template>
 
 <script>
