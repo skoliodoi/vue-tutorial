4716a3fc08fb0ee3f3ff8fdd838630e50dd6c24e
Szymon Zygaluk <szymon.zygaluk@gmail.com>
2020-08-05 14:24:14 +0200

    Exercise Done

--- Module 9 - Advanced Components Usage/Exercise/src/App.vue
+++ Module 9 - Advanced Components Usage/Exercise/src/App.vue
@@ -3,13 +3,15 @@
         <div class="row">
             <div class="col-xs-12">
                 <br>
-                <button class="btn btn-primary">Load Blue Template</button>
-                <button class="btn btn-success">Load Green Template</button>
-                <button class="btn btn-danger">Load Red Template</button>
+                <button class="btn btn-primary" @click="makeBlue">Load Blue Template</button>
+                <button class="btn btn-success"
+                @click="makeGreen">Load Green Template</button>
+                <button class="btn btn-danger"
+                @click="makeRed">Load Red Template</button>
                 <hr>
-                <app-blue></app-blue>
-                <app-green></app-green>
-                <app-red></app-red>
+                <component :is="showComponent">
+                  <h1>This component is {{ textColor }}</h1>
+                </component>
             </div>
         </div>
     </div>
@@ -25,6 +27,26 @@
             appBlue: Blue,
             appGreen: Green,
             appRed: Red
+        },
+        data() {
+          return {
+            showComponent: "",
+            textColor: ""
+          }
+        },
+        methods: {
+          makeBlue() {
+            this.showComponent='app-blue';
+            this.textColor = "Blue!"
+          },
+          makeGreen() {
+            this.showComponent='app-green';
+            this.textColor = "Green!"
+          },
+          makeRed() {
+            this.showComponent='app-red';
+            this.textColor = "Red!"
+          }
         }
     }
 </script>
