73807538a7d04bc1ed35941cee54ee87ee8a8521
Szymon Zygaluk <szymon.zygaluk@gmail.com>
2020-08-06 10:06:07 +0200

    Adding new quotes

--- Module 10 - Second Project - Wonderful Quotes/src/App.vue
+++ Module 10 - Second Project - Wonderful Quotes/src/App.vue
@@ -1,11 +1,18 @@
 <template>
     <div class="container">
+       <new-quote @quoteCreated="addQuote"></new-quote>
       <quote-grid :myQuotes="quotes">
       </quote-grid>
+      <div class="row">
+        <div class="col-sm-12 text-center">
+          <div class="alert alert-info">Info: Click on a Quote to delete it!</div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
+  import NewQuote from './components/NewQuote.vue'
   import QuoteGrid from './components/QuoteGrid.vue'
   export default {
     data() {
@@ -18,7 +25,13 @@
       }
     },
     components: {
-      quoteGrid: QuoteGrid
+      quoteGrid: QuoteGrid,
+      newQuote: NewQuote
+    },
+    methods: {
+      addQuote(data) {
+        this.quotes.push(data)
+      }
     }
   }
 </script>
--- Module 10 - Second Project - Wonderful Quotes/src/components/Quote.vue
+++ Module 10 - Second Project - Wonderful Quotes/src/components/Quote.vue
@@ -2,7 +2,9 @@
   <div class="col-sm-6 col-md-4 col lg-3">
     <div class="panel panel-default">
       <div class="panel-body quote">
-        <slot></slot>
+        <slot>
+
+        </slot>
       </div>
     </div>
   </div>
