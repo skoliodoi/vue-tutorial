c2069ce372899e65d1d296334e42abc7bff26750
Szymon Zygaluk <szymon.zygaluk@gmail.com>
2020-08-18 13:57:47 +0200

    Binding <textarea> and Saving Line Breaks

--- Module 11 - Handling User Input with Forms/src/App.vue
+++ Module 11 - Handling User Input with Forms/src/App.vue
@@ -39,7 +39,8 @@
                     <textarea
                             id="message"
                             rows="5"
-                            class="form-control"></textarea>
+                            class="form-control"
+                            v-model="message"></textarea>
                 </div>
             </div>
             <div class="row">
@@ -107,7 +108,8 @@
                         <p>Mail: {{ userData.email }}</p>
                         <p>Password: {{ userData.password }}</p>
                         <p>Age: {{ userData.age }}</p>
-                        <p>Message: </p>
+                        <p style="white-space: pre">Message:</p>
+                        <p style="white-space: pre">{{ message }}</p>
                         <p><strong>Send Mail?</strong></p>
                         <ul>
                             <li></li>
@@ -130,7 +132,8 @@
             email: '',
             password: '',
             age: 27
-          }
+          },
+          message: "A new text"
         }
       }
     }
