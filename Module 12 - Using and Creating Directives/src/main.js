6b91e04550112105fefc49d8eab4187cff999173
Szymon <szulanowski@gmail.com>
2020-08-22 14:37:12 +0200

    Modifying a Custom Directive with Modifiers

--- Module 12 - Using and Creating Directives/src/main.js
+++ Module 12 - Using and Creating Directives/src/main.js
@@ -4,11 +4,18 @@ import App from './App.vue'
 Vue.directive('highlight', {
   bind(el,binding, vnode) {
     //el.style.backgroundColor= binding.value
-    if (binding.arg == 'background') {
-      el.style.background = binding.value 
-    } else {
-      el.style.color = binding.value
+    var delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
     }
+
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.background = binding.value 
+      } else {
+        el.style.color = binding.value
+      }
+    } , delay)   
   }
 });
 
