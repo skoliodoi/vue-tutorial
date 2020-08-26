592c68a3a66a725b5e8816c164b3c4a8c7f461e1
Szymon <szulanowski@gmail.com>
2020-08-26 13:27:47 +0200

    An (often-times better) Alternative to Filters: Computed Properties

--- Module 13 - Improving your App with Filters and Mixins/src/App.vue
+++ Module 13 - Improving your App with Filters and Mixins/src/App.vue
@@ -7,6 +7,13 @@
                 <p>{{ generalGrievous | to-uppercase }}</p>
             </div>
         </div>
+        <hr>
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+            <input v-model="filteredText">
+            <ul>
+              <li v-for="each in filteredList">{{ each }}</li>
+            </ul>
+        </div>    
     </div>
 </template>
 
@@ -15,12 +22,21 @@
       data() {
         return {
           obiwanKenobi: "Hello there!",
-          generalGrievous: "General Kenobi"
+          generalGrievous: "General Kenobi",
+          characters: ['Qui-Gon Jinn', 'Han Solo', 'Luke Skywalker', 'Chewbacca'],
+          filteredText: ''
         }
       },
       filters: {
         'to-lowercase'(value) {
           return value.toLowerCase()
+        }
+      },
+      computed: {
+        filteredList() {
+          return this.characters.filter((element) => {
+            return element.match(this.filteredText)
+          })
         }
       }
     }
