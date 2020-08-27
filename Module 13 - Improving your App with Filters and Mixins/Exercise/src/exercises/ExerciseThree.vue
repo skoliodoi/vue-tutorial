56a19318a519f234a799f1a77f069dbd5957f442
Szymon <szulanowski@gmail.com>
2020-08-27 21:32:35 +0200

    Exercise 3 done!

--- Module 13 - Improving your App with Filters and Mixins/Exercise/src/exercises/ExerciseThree.vue
+++ Module 13 - Improving your App with Filters and Mixins/Exercise/src/exercises/ExerciseThree.vue
@@ -3,11 +3,27 @@
     <!-- Exercise 3 -->
     <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
     <p>Exercise 3</p>
+    <p>{{ reversed }}</p>
+    <p>{{ counted }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data(){
+    return {
+      reverseText: 'This text is gonna be reversed!',
+      countWords: 'Konstantynopolitańczykiewiczówianeczka'
+    }
+  },
+  computed: {
+    reversed() {
+      return this.reverseText.split("").reverse().join("")
+    },
+    counted() {
+      const wordsLen = this.countWords.length;
+      return this.countWords + " (" + wordsLen + ")"
+    }
+  }
 };
 </script>
