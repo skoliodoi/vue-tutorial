02bee199cb2154dd5a2452ea53f407a1ee30ab48
Szymon <szulanowski@gmail.com>
2020-08-27 21:22:20 +0200

    Exercise 2 done!

--- Module 13 - Improving your App with Filters and Mixins/Exercise/src/exercises/ExerciseTwo.vue
+++ Module 13 - Improving your App with Filters and Mixins/Exercise/src/exercises/ExerciseTwo.vue
@@ -4,11 +4,16 @@
     <!-- Build a global Filter which counts the length of a word and it appends it -->
     <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
     <p>Exercise 2</p>
+    <p>{{ text | count-length }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  
+  data() {
+    return {
+      text: 'Imponderabilia'
+    }
+  }
 }
 </script>
--- Module 13 - Improving your App with Filters and Mixins/Exercise/src/main.js
+++ Module 13 - Improving your App with Filters and Mixins/Exercise/src/main.js
@@ -1,14 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.filter('count-length', function(value) {
+  const textLen = value.length
+  return  value + " (" + textLen + ")"
+})
+
 Vue.filter('to-lowercase', function(value) {
     return value.toLowerCase();
-});
-
-Vue.mixin({
-    created() {
-        console.log('Global Mixin - Created Hook');
-    }
 });
 
 new Vue({
