56b05e71f5727ca92215b68e1633710a6c5f9cf1
Szymon <szulanowski@gmail.com>
2020-08-28 09:22:16 +0200

    Setting up the module & Preparing Code to use Transitions

---
+++ Module 14 - Adding Animations and Transitions/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                <h1>Animations</h1>
+                <hr>
+                <button class="btn btn-success"
+                @click="show=!show">Show alert</button>
+                <div class="alert alert-info info"
+                v-if="show">Some info</div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+              show: false
+            }
+        }
+    }
+</script>
+
+<style>
+ .info {
+   margin-top: 10px;
+ }
+</style>
