57317f0bccb235a761d94ee04178c2d744b9b68a
Szymon <szulanowski@gmail.com>
2020-09-08 19:53:59 +0200

    Creating Custom Resources

--- Module 15 - Connecting to Servers via Http - Using vue-resource/src/App.vue
+++ Module 15 - Connecting to Servers via Http - Using vue-resource/src/App.vue
@@ -45,7 +45,8 @@
           //   }, error => {
           //     console.log(error)
           //   });
-          this.resource.save({}, this.user);
+          // this.resource.save({}, this.user);
+          this.resource.saveAlt(this.user);
         },
         fetchData() {
           this.$http.get('data.json')
@@ -62,7 +63,10 @@
         }
       },
       created() {
-        this.resource = this.$resource('data.json');
+        const customActions = {
+          saveAlt: {method: 'POST', url: 'alternative.json'}
+        }
+        this.resource = this.$resource('data.json', {}, customActions);
       }
     }
 </script>
