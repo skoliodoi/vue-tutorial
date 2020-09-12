5925fdadf7b97a1da9eb6dcc7da5f78570d29d92
Szymon <szulanowski@gmail.com>
2020-09-12 18:48:37 +0200

    Navigating with Router Links

---
+++ Module 16 - Routing in a VueJS Application/src/components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+  <ul class="nav nav-pills">
+  <li class="nav-item">
+    <router-link to="/" class="nav-link">Home</router-link>
+  </li>
+  <li class="nav-item">
+    <router-link to="/user" class="nav-link">User</router-link>
+  </li>
+</ul>
+</template>
