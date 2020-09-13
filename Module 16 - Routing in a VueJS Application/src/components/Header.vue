3fb6e4797587bb527dbe1467ba6845a9639828d7
Szymon <szulanowski@gmail.com>
2020-09-13 19:08:31 +0200

    Setting Up Route Parameters

--- Module 16 - Routing in a VueJS Application/src/components/Header.vue
+++ Module 16 - Routing in a VueJS Application/src/components/Header.vue
@@ -3,7 +3,7 @@
     <router-link to="/" tag="li" active-class="active" exact>
     <a class="nav-link">Home</a>
     </router-link>
-    <router-link to="/user" tag="li" active-class="active" exact>
+    <router-link to="/user/10" tag="li" active-class="active" exact>
     <a class="nav-link">User</a>
     </router-link>
 </ul>
