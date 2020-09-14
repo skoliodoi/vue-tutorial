30b65a75cb210f7b1a3cc1e6b1bce82e9b3c1da6
Szymon <szulanowski@gmail.com>
2020-09-14 12:02:06 +0200

    Setting Up "Catch All" Routes / Wildcards

--- Module 16 - Routing in a VueJS Application/src/routes.js
+++ Module 16 - Routing in a VueJS Application/src/routes.js
@@ -18,5 +18,5 @@ export const routes = [
     { path: ':id', component: UserDetail },
     { path: ':id/edit', component: UserEdit, name: 'userEdit' }
   ] },
-  { path: '/redirect-me', redirect: '/user'}
+  { path: '*', redirect: "/"}
 ]
