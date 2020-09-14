9a8241db9419253131c07d624f004ff119821df9
Szymon <szulanowski@gmail.com>
2020-09-14 20:42:21 +0200

    Passing the Hash Fragment

--- Module 16 - Routing in a VueJS Application/src/components/user/UserDetail.vue
+++ Module 16 - Routing in a VueJS Application/src/components/user/UserDetail.vue
@@ -4,9 +4,29 @@
     <p>This User ID is {{ $route.params.id }}</p>
     <router-link
     tag="button"
-    :to="{ name: 'userEdit', params: { id: $route.params.id }, query: { local: 'en', q: 1000} }"
+    :to="link"
     class="btn btn-primary">
       Edit User
     </router-link>
   </div>
 </template>
+
+<script>
+  export default {
+   data() {
+     return {
+       link: {
+         name: 'userEdit', 
+         params: { 
+           id: this.$route.params.id 
+          }, 
+         query: { 
+           local: 'en', 
+           q: 1000 
+          },
+          hash: "#data"
+       }
+     }
+   } 
+  }
+</script>
