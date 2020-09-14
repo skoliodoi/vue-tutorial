9529ce0dd273f2bdb35174b8c2b38948b0ac7fe8
Szymon <szulanowski@gmail.com>
2020-09-14 20:49:46 +0200

    Controlling the Scroll Behavior

--- Module 16 - Routing in a VueJS Application/src/components/user/UserEdit.vue
+++ Module 16 - Routing in a VueJS Application/src/components/user/UserEdit.vue
@@ -3,5 +3,7 @@
     <h3>Edit the User</h3>
     <p>Local: {{ $route.query.local }}</p>
     <p>Analytics: {{ $route.query.q }}</p>
+    <div style="height: 700px"></div>
+    <p id="data">Extra Data</p>
   </div>
 </template>
