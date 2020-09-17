719d151fbc98cb093490d8c81fd7b459d873cb85
Szymon <szulanowski@gmail.com>
2020-09-17 21:24:27 +0200

    Using the "beforeLeave" Guard

--- Module 16 - Routing in a VueJS Application/src/components/user/UserEdit.vue
+++ Module 16 - Routing in a VueJS Application/src/components/user/UserEdit.vue
@@ -4,8 +4,29 @@
     <p>Local: {{ $route.query.local }}</p>
     <p>Analytics: {{ $route.query.q }}</p>
     <hr>
-    <button class="btn btn-primary">Confirm</button>
+    <button class="btn btn-primary" 
+    @click="confirmed= true">Confirm</button>
     <div style="height: 700px"></div>
     <p id="data">Extra Data</p>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      confirmed: false
+    }
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.confirmed) {
+      next();
+    } else {
+      if (confirm('Are you sure?')) {
+        next();
+      } else {
+        next(false);
+      }
+    }
+  }
+}
+</script>
