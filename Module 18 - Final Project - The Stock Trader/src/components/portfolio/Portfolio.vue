2eb73e8dff5fecca1f0740dddd669f81a2357b77
Szymon <szulanowski@gmail.com>
2020-09-25 22:29:10 +0200

    Connecting the Portfolio with Vuex

--- Module 18 - Final Project - The Stock Trader/src/components/portfolio/Portfolio.vue
+++ Module 18 - Final Project - The Stock Trader/src/components/portfolio/Portfolio.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1>The Portfolio Component</h1>
+  <div>
+    <app-stock v-for="stock in stocks"
+    :stock="stock"></app-stock>
+  </div>
 </template>
 
 <script>
+  import Stock from './Stock.vue'
   export default {
-    
+    computed: {
+      stocks() {
+        return this.$store.getters.stockPortfolio
+      }
+    },
+    components: {
+      appStock: Stock
+    }
   }
 </script>
 
