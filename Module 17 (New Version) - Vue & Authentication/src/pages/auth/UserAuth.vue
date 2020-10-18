65f2a7eebd0901bdfe106f7632eb1b4ea45765a6
Szymon <szulanowski@gmail.com>
2020-10-18 22:59:27 +0200

    Adding an Authentication Page (Login & Signup)

--- Module 17 (New Version) - Vue & Authentication/src/pages/auth/UserAuth.vue
+++ Module 17 (New Version) - Vue & Authentication/src/pages/auth/UserAuth.vue
@@ -1,31 +1,79 @@
 <template>
-  <form action="">
-    <base-card>
-    <div class="form-control">
-      <label for="email">Email</label>
-      <input type="email" id="email">
-    </div>
-    <div class="form-control">
-      <label for="password">Password</label>
-      <input type="password" id="password">
-    </div>
-    <base-button>Login</base-button>
-    <base-button type="button" mode="flat">Signup instead</base-button>
-    </base-card>
-  </form>
+  <base-card>
+    <form @submit.prevent="submitForm">
+      <div class="form-control">
+        <label for="email">Email</label>
+        <input type="email" id="email" v-model.trim="email" />
+      </div>
+      <div class="form-control">
+        <label for="password">Password</label>
+        <input type="password" id="password" v-model.trim="password" />
+      </div>
+      <p v-if="!formIsValid">
+        Please enter a valid email and password (must be at least 6 characters
+        long!
+      </p>
+      <base-button>{{submitButtonCaption}}</base-button>
+      <base-button type="button" mode="flat" @click="switchMode"
+        >{{switchModeButtonCaption}}</base-button
+      >
+    </form>
+  </base-card>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      email: "",
+      password: "",
+      formIsValid: true,
+      mode: "login",
+    };
+  },
+  computed: {
+    submitButtonCaption() {
+      if (this.mode === "login") {
+        return "Login";
+      } else {
+        return "Signup";
+      }
+    },
+    switchModeButtonCaption() {
+      if (this.mode === "login") {
+        return "Signup instead";
+      } else {
+        return "Login instead";
+      }
+    },
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true;
+      if (
+        this.email === "" ||
+        this.email.includes("@") ||
+        this.password.length < 6
+      ) {
+        this.formIsValid = false;
+        return;
+      }
+    },
+    switchMode() {
+      if (this.mode === "login") {
+        this.mode = "signup";
+      } else {
+        this.mode = "login";
+      }
+    },
+  },
+};
 </script>
 
 <style>
 form {
   margin: 1rem;
   border: 1px solid #ccc;
-  border-radius: 12px;
   padding: 1rem;
 }
 
