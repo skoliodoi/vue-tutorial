17b53c3544275f346dd978b89808ccc817dc8b5a
Szymon <szulanowski@gmail.com>
2020-10-19 20:15:45 +0200

    Adding a "Login" Action & Flow

--- Module 17 (New Version) - Vue & Authentication/src/store/modules/auth/actions.js
+++ Module 17 (New Version) - Vue & Authentication/src/store/modules/auth/actions.js
@@ -23,5 +23,30 @@ export default {
       userId: responseData.localId,
       tokenExpiration: responseData.expiresIn
     })
+  },
+  async login(context, payload)  {
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyD2bhCfhkmCMpBueYLSpXKlANL043u9-3M',{
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    })
+
+    const responseData = await response.json();
+
+    if (!response.ok){
+      console.log(responseData)
+      const error = new Error(responseData.message || "Failed to authenticate!");
+      throw error;
+    }
+
+    console.log(responseData)
+    context.commit('setUser',{
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    })
   }
 }
