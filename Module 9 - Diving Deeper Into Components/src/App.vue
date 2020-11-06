efb4b36f51ea46f8a860868ab219b5dc5884e167
Szymon <szulanowski@gmail.com>
2020-11-06 14:51:45 +0100

    Relearning about slots part 4: Scoped Slots

--- Module 9 - Diving Deeper Into Components/src/App.vue
+++ Module 9 - Diving Deeper Into Components/src/App.vue
@@ -7,25 +7,31 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps["another-prop"] }}</p>
+    </course-goals>
   </div>
 </template>
 
 <script>
-import UserInfo from './components/UserInfo.vue'
-import TheHeader from './components/TheHeader.vue'
-import BadgeList from './components/BadgeList.vue'
+import CourseGoals from "./components/CourseGoals.vue";
+import UserInfo from "./components/UserInfo.vue";
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
 export default {
   components: {
-    'the-header': TheHeader,
-    'badge-list': BadgeList,
-    UserInfo
+    "the-header": TheHeader,
+    "badge-list": BadgeList,
+    UserInfo,
+    CourseGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
---
+++ Module 9 - Diving Deeper Into Components/src/components/CourseGoals.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul>
+    <li v-for="goal in goals" :key="goal">
+      <slot :item="goal" another-prop="..."></slot>
+    </li>
+  </ul>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      goals: ['Finish the course', 'Leave my shitty job']
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
