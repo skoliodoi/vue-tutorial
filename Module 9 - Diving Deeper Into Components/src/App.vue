c40f606d2e49ac0e7a8b73f1dd73fadc9a2e7a9c
Szymon <szulanowski@gmail.com>
2020-11-06 15:00:42 +0100

    Relearning about dynamic components part 1: Dynamic Components intro

--- Module 9 - Diving Deeper Into Components/src/App.vue
+++ Module 9 - Diving Deeper Into Components/src/App.vue
@@ -1,33 +1,35 @@
 <template>
   <div>
     <the-header></the-header>
-    <badge-list></badge-list>
-    <user-info
-      :full-name="activeUser.name"
-      :info-text="activeUser.description"
-      :role="activeUser.role"
-    ></user-info>
-    <course-goals #default="slotProps">
-        <h2>{{ slotProps.item }}</h2>
-        <p>{{ slotProps["another-prop"] }}</p>
-    </course-goals>
+    <button @click="setSelectedComponent('active-goals')">Active goals</button>
+    <button @click="setSelectedComponent('manage-goals')">Manage goals</button>
+    <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
+    <manage-goals v-if="selectedComponent === 'manage-goals'"></manage-goals> -->
+    <component :is="selectedComponent">
+
+    </component>
   </div>
 </template>
 
 <script>
-import CourseGoals from "./components/CourseGoals.vue";
-import UserInfo from "./components/UserInfo.vue";
+// import CourseGoals from "./components/CourseGoals.vue";
+// import UserInfo from "./components/UserInfo.vue";
 import TheHeader from "./components/TheHeader.vue";
-import BadgeList from "./components/BadgeList.vue";
+// import BadgeList from "./components/BadgeList.vue";
+import ActiveGoals from './components/ActiveGoals.vue';
+import ManageGoals from './components/ManageGoals.vue';
 export default {
   components: {
     "the-header": TheHeader,
-    "badge-list": BadgeList,
-    UserInfo,
-    CourseGoals,
+    ActiveGoals,
+    ManageGoals
+    // "badge-list": BadgeList,
+    // UserInfo,
+    // CourseGoals,
   },
   data() {
     return {
+      selectedComponent: 'active-goals',
       activeUser: {
         name: "Maximilian Schwarzmüller",
         description: "Site owner and admin",
@@ -35,6 +37,11 @@ export default {
       },
     };
   },
+  methods: {
+    setSelectedComponent(cmp) {
+      this.selectedComponent = cmp
+    }
+  }
 };
 </script>
 
