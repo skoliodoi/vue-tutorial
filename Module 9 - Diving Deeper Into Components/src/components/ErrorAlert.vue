d7505150c0b120937589e3d2a6354dbf47bacf87
Szymon <szulanowski@gmail.com>
2020-11-06 15:22:49 +0100

    Applying What We Know & A Problem

---
+++ Module 9 - Diving Deeper Into Components/src/components/ErrorAlert.vue
@@ -0,0 +1,23 @@
+<template>
+  <dialog open>
+    <slot></slot>
+  </dialog>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  dialog {
+    position: fixed;
+    top: 20vh;
+    left: 30%;
+    width: 40%;
+    background-color: whitesmoke;
+    box-shadow: 0 2px 8px rgba(0,0,0,0.26);
+    padding: 1rem;
+  }
+</style>
--- Module 9 - Diving Deeper Into Components/src/components/ManageGoals.vue
+++ Module 9 - Diving Deeper Into Components/src/components/ManageGoals.vue
@@ -1,6 +1,33 @@
 <template>
 <div>
   <h2>Manage Goals</h2>
-  <input type="text" />
+  <input type="text" ref="goal"/>
+  <button @click="setGoal">Set Goal</button>
+  <error-alert v-if="inputInvalid">
+    <h2>Input invalid!</h2>
+    <p>Write down something!</p>
+  </error-alert>
 </div>
 </template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+export default {
+  components: {
+    ErrorAlert
+  },
+  data(){
+    return {
+      inputInvalid: false,
+    }
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputInvalid = true
+      }
+    }
+  }
+}
+</script>
