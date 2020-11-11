0e74b1fa2034a2a175ce5d8f0765118236892f7a
Szymon <szulanowski@gmail.com>
2020-11-11 21:55:21 +0100

    Vue forms recap part 3: v-model and Dropdowns

--- Module 25 - Forms Recap (Vue3)/src/components/TheForm.vue
+++ Module 25 - Forms Recap (Vue3)/src/components/TheForm.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="form-control">
       <label for="referrer">How did you hear about us?</label>
-      <select id="referrer" name="referrer">
+      <select id="referrer" name="referrer" v-model="referrer">
         <option value="google">Google</option>
         <option value="wom">Word of mouth</option>
         <option value="newspaper">Newspaper</option>
@@ -57,7 +57,8 @@ export default {
   data() {
     return {
       userName: '',
-      userAge: null
+      userAge: null,
+      referrer: 'wom',
     }
   },
   methods: {
@@ -68,6 +69,8 @@ export default {
       console.log(this.userAge);
       console.log(69)
       this.userAge = null;
+      console.log(this.referrer);
+      this.referrer="wom"
     }
   }
 }
