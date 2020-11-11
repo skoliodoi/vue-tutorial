56ffeab6fd4f88443f1786e115df08d811028805
Szymon <szulanowski@gmail.com>
2020-11-11 22:38:08 +0100

    Vue forms recap part 6: Building a Custom Control Component

--- Module 25 - Forms Recap (Vue3)/src/components/TheForm.vue
+++ Module 25 - Forms Recap (Vue3)/src/components/TheForm.vue
@@ -65,6 +65,9 @@
         <label for="how-other">Other</label>
       </div>
     </div>
+    <div class="form-contro">
+      <rating-control></rating-control>
+    </div>
     <div class="form-control">
       <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm"/>
       <label for="confirm-terms">Agree to terms?</label>
@@ -76,7 +79,11 @@
 </template>
 
 <script>
+import RatingControl from './RatingControl.vue'
 export default {
+  components: {
+    RatingControl
+  },
   data() {
     return {
       userName: "",
