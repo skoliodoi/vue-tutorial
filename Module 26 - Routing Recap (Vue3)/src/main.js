5eb16608de41516754ec5104835af31617c8232e
Szymon <szulanowski@gmail.com>
2020-11-12 18:19:18 +0100

    Vue Routing recap part 12: More Fun with Named Routes & Location Objects

--- Module 26 - Routing Recap (Vue3)/src/main.js
+++ Module 26 - Routing Recap (Vue3)/src/main.js
@@ -12,10 +12,11 @@ const router = createRouter({
   routes: [
     { path: '/', redirect: '/teams' },
     {
+      name: 'teams',
       path: '/teams', 
       component: TeamsList, 
       children: [
-        { path: ':teamId', component: TeamMembers, props: true }
+        { name: 'team-members', path: ':teamId', component: TeamMembers, props: true }
       ]
     },
     { path: '/users', component: UsersList },
