955c8dd43ed49dd49f9e13b067f18de90c63ccde
Szymon <szulanowski@gmail.com>
2020-11-13 17:32:39 +0100

    Vue Routing recap part 14: Rendering Multiple Routes with Named Router Views

---
+++ Module 26 - Routing Recap (Vue3)/src/components/users/UsersFooter.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>Users Footer</h2>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- Module 26 - Routing Recap (Vue3)/src/main.js
+++ Module 26 - Routing Recap (Vue3)/src/main.js
@@ -5,7 +5,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue'
+import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
@@ -13,13 +15,13 @@ const router = createRouter({
     { path: '/', redirect: '/teams' },
     {
       name: 'teams',
-      path: '/teams', 
-      component: TeamsList, 
+      path: '/teams',
+      components: { default: TeamsList, footer: TeamsFooter },
       children: [
         { name: 'team-members', path: ':teamId', component: TeamMembers, props: true }
       ]
     },
-    { path: '/users', component: UsersList },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
     { path: '/:catchAll(.*)', redirect: '/teams' }
   ]
 })
