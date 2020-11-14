ce9ebb4d5e9576588a107d1f040ad01ee4a5a7c0
Szymon <szulanowski@gmail.com>
2020-11-14 19:12:46 +0100

    Vue Animations recap part 4: CSS Animations with the Transition Component

--- Module 27 - Animations Recap (Vue 3)/src/App.vue
+++ Module 27 - Animations Recap (Vue 3)/src/App.vue
@@ -48,31 +48,31 @@ export default {
 
 <style>
 .v-enter-from {
-  opacity: 0;
-  transform: translateY(-30px);
+  /* opacity: 0;
+  transform: translateY(-30px); */
 }
 
 .v-enter-active {
-  transition: all 0.3s ease-out;
+  animation: slide-scale 0.3s ease-out;
 }
 
 .v-enter-to {
-  opacity: 1;
-  transform: translateY(0);
+  /* opacity: 1;
+  transform: translateY(0); */
 }
 
 .v-leave-from {
-  opacity: 1;
-  transform: translateY(0);
+  /* opacity: 1;
+  transform: translateY(0); */
 }
 
 .v-leave-active {
-  transition: all 0.3s ease-out;
+  animation: slide-scale 0.3s ease-out;
 }
 
 .v-leave-to {
-  opacity: 0;
-  transform: translateY(30px);
+  /* opacity: 0;
+  transform: translateY(30px); */
 }
 
 
@@ -123,7 +123,7 @@ button:active {
   border-radius: 12px;
 }
 
-@keyframes slide-fade {
+@keyframes slide-scale {
   0% {
     transform: translateX(0) scale(1);
   } 
